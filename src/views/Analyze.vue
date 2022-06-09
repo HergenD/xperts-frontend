3fc60bcb495f1221d48856b2d53002340261099b
seung ah pyun <seungahpyun@icloud.com>
2022-06-09 12:15:02 +0200

    font size

--- src/views/Analyze.vue
+++ src/views/Analyze.vue
@@ -9,7 +9,7 @@
         <h1 class="display-2 font-weight-bold mb-3">
            👩‍⚕️
         </h1>
-        <h2 style="font-size:36px;font-weight:600;font-family: 'Press Start 2P', cursive !important;">X-perts Dental AI</h2>
+        <h2 style="font-size:38px;font-weight:600;font-family: 'Press Start 2P', cursive !important;">X-perts Dental AI</h2>
       </v-col>
     </v-row>
     <v-row class="text-center">
