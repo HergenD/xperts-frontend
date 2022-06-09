0136b4ea7c16fbd2814723343d8495c81e5893ae
seung ah pyun <seungahpyun@icloud.com>
2022-06-09 12:56:16 +0200

    chnage api

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -87,7 +87,7 @@ export default {
       let fd = new FormData()
       fd.append('file', this.image)
 
-      axios.post('https://xperts.hergen.nl/image', fd)
+      axios.post('https://xperts-yjhmvqcmwa-ez.a.run.app/image', fd)
           .then(resp => {
             console.log(resp.data)
 
